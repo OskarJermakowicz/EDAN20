330ff7c1854127ad1bb143313f89546a0ce3f860
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-09-04 12:29:56 +0200

    Master index done

--- Assignment1/indexer.py
+++ Assignment1/indexer.py
@@ -13,28 +13,32 @@ def get_files(dir, suffix):
 
 # The program that will be run
 def program(dir):
-    for file_name in get_files(dir, 'txt'):
-        # Dictionary where the indexing will be stored
-        index_dict = {}
+    # Master index structure: word: { textfile: [pos1,pos2,...] }
+    master_index = {}
 
+    for file_name in get_files(dir, 'txt'):
         # Import the file to index
         fi = dir + "/" + file_name
         with open(fi) as f:
             pattern = '([a-zA-Z]+\w+)'
-            regex = re.compile(pattern, re.IGNORECASE)
+            regex = re.compile(pattern, re.IGNORECASE | re.U)
             for match in regex.finditer(f.read()):
-                if match.group(1).lower() in index_dict:
-                    index_dict[match.group(1).lower()].append(match.start())
+                if match.group(1).lower() in master_index:
+                    if file_name in master_index[match.group(1).lower()]:
+                        master_index[match.group(1).lower()][file_name].append(match.start())
+                    else:
+                        master_index[match.group(1).lower()][file_name] = [match.start()]
                 else:
-                    index_dict[match.group(1).lower()] = [match.start()]
+                    master_index[match.group(1).lower()] = { file_name: [match.start()] }
 
             # Save the indexing to a idx file
             with open('file_name.idx', 'w') as f:
-                f.write(str(index_dict))
-                # pickle.dump(str(index_dict), f)
+                f.write(str(master_index))
+                # pickle.dump(str(master_index), f)
 
-            # Test print
-            print(index_dict)
+    # Test print
+    print(master_index['samlar'])
+    print(master_index['ände'])
 
 # Run the indexing program
 program(sys.argv[1])
