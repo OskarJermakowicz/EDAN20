bd8a2fbdbae4b1812fa6508f6797fe8b1d9e4e6a
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-09-07 16:06:39 +0200

    Added printout program and refractored tokenization.py

--- Assignment2/tokenization.py
+++ Assignment2/tokenization.py
@@ -7,9 +7,8 @@ import codecs
 import time
 import regex as re
 
-# The program that will be run
-def program(file_name):
-    word_count = 0
+def tokenize(file_name):
+    word_count = {}
     new_text = ""
 
     # Open the corpus
@@ -17,28 +16,57 @@ def program(file_name):
         # Go through each sentence
         pattern = '((\s)?+[^.!?]*[.!?])'
         regex = re.compile(pattern, re.IGNORECASE | re.U)
-        for sentence_match in regex.finditer(f.read()):
+        for sentence in regex.finditer(f.read()):
             new_text += "<s>"
-            new_text += sentence_match.group(1).replace('.', ' ').replace('\n', '').lower()
+            new_text += sentence.group(1).replace('.', ' ').replace('\n', '').lower()
             new_text += "</s>\n"
 
             # Also go through the words in the sentence.
             pattern = '(\p{L}+)'
             regex = re.compile(pattern, re.IGNORECASE | re.U)
-            for word_match in regex.finditer(sentence_match.group(1)):
-                word_count += 1
+            for word in regex.finditer(sentence.group(1)):
+                if word.group(1).lower() in word_count:
+                    word_count[word.group(1).lower()] += 1
+                else:
+                    word_count[word.group(1).lower()] = 1
+
+    return [word_count, new_text]
+
+def print_analysis(sentence, word_count):
+    print("\nUnigrams - \"", sentence, "\"")
+    print("====================================================================")
+    print('%-12s%-12s%-12s%-12s' % ("wi", "C(wi)", "#words", "P(wi)"))
+    print("====================================================================")
+    pattern = '(\p{L}+)'
+    regex = re.compile(pattern, re.IGNORECASE | re.U)
+    for word in regex.finditer(sentence):
+        print('%-12s%-12s%-12s%-12s' % (word.group(1).lower(), word_count[word.group(1).lower()], sum(tok[0].values()), "P(wi)"))
+    print("====================================================================")
+    print("Prob. unigrams:", "n/a", "Entropy rate:", "n/a", "Perplexity:", "n/a")
 
 
-
-    # Print new text to a file
-    with open('result.txt', 'w') as f:
-        f.write(new_text)
-
-    """ Test prints """
-    print("\n--- Number of words ---")
-    print(word_count)
+    print("\nBigrams - \"", sentence, "\"")
+    print("====================================================================")
+    print('%-12s%-12s%-12s%-12s%-12s' % ("wi", "wi+1", "Ci,i+1", "C(i)", "P(wi+1|wi)"))
+    print("====================================================================")
+    pattern = '(\p{L}+)'
+    regex = re.compile(pattern, re.IGNORECASE | re.U)
+    for word in regex.finditer(sentence):
+        print('%-12s%-12s%-12s%-12s%-12s' % (word.group(1).lower(), "wi+1", "Ci,i+1", "C(i)", "P(wi+1|wi)"))
+    print("====================================================================")
+    print("Prob. bigrams:", "n/a", "Entropy rate:", "n/a", "Perplexity:", "n/a")
 
 # Run the indexing program
 start_time = time.time()
-program(sys.argv[1])
+
+tok = tokenize(sys.argv[1])
+# Print new text to a file
+with open('result.txt', 'w') as f:
+    f.write(tok[1])
+
+print("\n--- Number of words in", sys.argv[1], "---")
+print(sum(tok[0].values()))
+
+print_analysis("Det var en gång en katt som hette Nils </s>", tok[0])
+
 print("\n--- Execution time: %s seconds ---" % (time.time() - start_time))
