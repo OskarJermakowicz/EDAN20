6c12f7648e9cb6a358bb91fcc12e97d9af7d5cbe
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-09-19 15:34:49 +0200

    Reaches 92.65 F score now

--- Assignment3/ml_chunker.py
+++ Assignment3/ml_chunker.py
@@ -72,10 +72,9 @@ def extract_features_sent(sentence, w_size, feature_names):
         for j in range(2 * w_size + 1):
             x.append(padded_sentence[i + j][1])
         # The chunks (Up to the word)
-        """
         for j in range(w_size):
-            feature_line.append(padded_sentence[i + j][2])
-        """
+            x.append(padded_sentence[i + j][2])
+
         # We represent the feature vector as a dictionary
         X.append(dict(zip(feature_names, x)))
         # The classes are stored in a list
@@ -148,8 +147,8 @@ if __name__ == '__main__':
     test_corpus = 'test.txt'
     w_size = 2  # The size of the context window to the left and right of the word
     feature_names = ['word_n2', 'word_n1', 'word', 'word_p1', 'word_p2',
-                     'pos_n2', 'pos_n1', 'pos', 'pos_p1', 'pos_p2']
-                     #'pchunk_n2', 'pchunk_n1']
+                     'pos_n2', 'pos_n1', 'pos', 'pos_p1', 'pos_p2',
+                     'pchunk_n2', 'pchunk_n1']
 
     train_sentences = conll_reader.read_sentences(train_corpus)
 
