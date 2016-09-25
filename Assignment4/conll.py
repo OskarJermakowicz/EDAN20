a48913caa780065c21af1f1d0ff923f8280ed5b4
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-09-25 13:56:44 +0200

    Implemented extractino of subject-verb-object pairs and triples

--- Assignment4/conll.py
+++ Assignment4/conll.py
@@ -3,7 +3,7 @@ CoNLL-X and CoNLL-U file readers and writers
 """
 __author__ = "Pierre Nugues"
 
-import os, time
+import os, time, codecs
 
 
 def get_files(dir, suffix):
@@ -31,8 +31,9 @@ def read_sentences(file):
     :param file:
     :return:
     """
-    f = open(file).read().strip()
-    sentences = f.split('\n\n')
+    with codecs.open(file, 'r', 'utf-8') as f:
+        content = f.read().strip()
+        sentences = content.split('\n\n')
     return sentences
 
 
@@ -73,6 +74,42 @@ def save(file, formatted_corpus, column_names):
         f_out.write('\n')
     f_out.close()
 
+def find_ssvb_pairs(formatted_corpus):
+    # Key: tuple of the pair, Value: how many times it occurs
+    rep = {}
+
+    for sentence in formatted_corpus:
+        for word in sentence:
+            if word['deprel'] == 'SS':
+                subject = word['form'].lower()
+                verb = sentence[int(word['head'])]['form'].lower()
+                if (subject, verb) not in rep:
+                    rep[(subject, verb)] = 1
+                else:
+                    rep[(subject, verb)] += 1
+    return rep
+
+def find_ssvbobj_triples(formatted_corpus):
+    # Key: tuple of the triple, Value: how many times it occurs
+    rep = {}
+
+    for sentence in formatted_corpus:
+        for word in sentence:
+            if word['deprel'] == 'SS':
+                subject = word['form'].lower()
+                verb = sentence[int(word['head'])]['form'].lower()
+
+                for pobj in sentence:
+                    if pobj['deprel'] == 'OO' and pobj['head'] == word['head']:
+                        object = pobj['form'].lower()
+                        if (subject, verb, object) not in rep:
+                            rep[(subject, verb, object)] = 1
+                        else:
+                            rep[(subject, verb, object)] += 1
+
+    return rep
+
+
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -85,15 +122,22 @@ if __name__ == '__main__':
     sentences = read_sentences(train_file)
     formatted_corpus = split_rows(sentences, column_names_2006)
     print(train_file, len(formatted_corpus))
-    print(formatted_corpus[0])
+    print(formatted_corpus[15])
 
-    #column_names_u = ['id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc']
+    ssvb_pairs = find_ssvb_pairs(formatted_corpus)
+    print("\n--- Total amount of pairs ---")
+    print(sum(ssvb_pairs.values()))
 
-    #files = get_files('ud-treebanks-v1.3', 'train.conllu')
-    #for train_file in files:
-        #sentences = read_sentences(train_file)
-        #formatted_corpus = split_rows(sentences, column_names_u)
-        #print(train_file, len(formatted_corpus))
-        #print(formatted_corpus[0])
+    print("\n--- Five most common subject-verb pairs ---")
+    for pair in sorted(ssvb_pairs, key=ssvb_pairs.get, reverse=True)[:5]:
+        print(ssvb_pairs[pair], "\t", pair[0], pair[1])
+
+    ssvbobj_triples = find_ssvbobj_triples(formatted_corpus)
+    print("\n--- Total amount of triples ---")
+    print(sum(ssvbobj_triples.values()))
+
+    print("\n--- Five most common subject-verb-object triples ---")
+    for triple in sorted(ssvbobj_triples, key=ssvbobj_triples.get, reverse=True)[:5]:
+        print(ssvbobj_triples[triple], "\t", triple[0], triple[1], triple[2])
 
     print("\n--- Execution time: %s seconds ---" % (time.time() - start_time))
